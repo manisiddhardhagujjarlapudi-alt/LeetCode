a4245b7caf236328a9af18474e144162d12af2a2
Mani Siddhardha <manisiddhardhagujjarlapudi@gmail.com>
2026-03-20 11:07:09 +0530

    Time: 3 ms (14.65%), Space: 12.3 MB (75.88%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,8 +1,8 @@
 class Solution(object):
     def numJewelsInStones(self, jewels, stones):
-        count = 0
-        for i in range(len(jewels)):
-            for j in range(len(stones)):
-                if jewels[i] == stones[j]:
-                    count += 1
-        return count
+        ans = 0
+        for i in jewels:
+            for j in stones:
+                if i == j:
+                    ans += 1
+        return ans         
