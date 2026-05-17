8068270656cefc9bcddcfc3d443b0f6a08893b74
Mani Siddhardha <manisiddhardhagujjarlapudi@gmail.com>
2026-05-17 10:58:37 +0530

    Time: 29 ms (5.53%), Space: 42.8 MB (22.93%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,10 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+    long root = 0;
+        for(long i = 1; i <= x ; i++){
+            if(i*i > x) break;
+            root = i;  
+        }
+        return (int)root;
     }
 }
