658144abf819af3c4b85c503bf9e42e9b62f34d6
Mani Siddhardha <manisiddhardhagujjarlapudi@gmail.com>
2026-06-13 14:40:22 +0530

    Time: 3 ms (87.64%), Space: 43 MB (84.48%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean sameStyle(char top,char ch){
+        if(top == '{' && ch == '}') return true;
+        if(top == '(' && ch == ')') return true;
+        if(top == '[' && ch == ']') return true;
+        return false;
+    }
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0;i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }else{
+                if(st.size()== 0) return false;
+                char top = st.peek();
+                if(sameStyle(top,ch)) st.pop();
+                else return false;
+            }
+        }
+        return (st.size()==0);
+    }
+}
