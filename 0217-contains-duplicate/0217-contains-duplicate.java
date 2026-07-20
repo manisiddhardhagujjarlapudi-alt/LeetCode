3c7708e7d363a083b5f682d3ca60de2a93048f5d
Mani Siddhardha <manisiddhardhagujjarlapudi@gmail.com>
2026-07-20 21:04:30 +0530

    Time: 26 ms (8.59%), Space: 81.2 MB (84.4%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] arr) {
+        int n = arr.length;
+        Arrays.sort(arr);
+        if(arr.length == 2 && arr[0] == arr[1]) return true;
+        boolean flag = false;
+        for(int i = 1; i < n-1; i++){
+            if(arr[i] == arr[i-1] || arr[i]== arr[i+1]){
+                flag = true;
+                break;
+            }
+        }
+        return flag;
+    }
+}
